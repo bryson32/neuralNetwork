fc638be3280bf49f40c5b9e068d255d2f28edb04
bryson32 <154479610+bryson32@users.noreply.github.com>
2024-12-22 22:08:25 -0800

    Introduce Batches, TRANSPOSE matrix, fixing shape error

--- neuron1.py
+++ neuron1.py
@@ -3,14 +3,17 @@ import numpy as np
 import matplotlib
 
 
-inputs = [1, 2, 3, 2.5]
+inputs = [[1, 2, 3, 2.5],    #batch of 3
+          [2.0, 5.0, -1.0, 2.0],
+          [-1.5, 2.7, 3.3, -0.8]]
+
 weights = [[0.2, 0.8, -0.5, 1.0],
            [0.5, -0.91, 0.26, -0.5],
            [-0.26, -0.27, 0.17, 0.87]]
 
 biases = [2, 3, 0.5]
 
-layer_outputs = np.dot(weights, inputs) + biases
+layer_outputs = np.dot(inputs, np.array(weights).T) + biases
 print(layer_outputs) 
 
 # layer_outputs = []  # Output of current layer
