91714d16b1eff4beff691c15a51a830dbf6a42f3
bryson32 <154479610+bryson32@users.noreply.github.com>
2024-12-22 18:00:46 -0800

    4 inputs on 3 neurons

--- neuron1.py
+++ neuron1.py
@@ -2,10 +2,20 @@ import sys
 import numpy as np
 import matplotlib
 
-inputs = [1.2, 5.1, 2.1]
-weights = [3.1, 2.1, 8.7]
-bias = 3   #this specific neurons bias
+inputs = [1, 2, 3, 2.5]
 
-output = inputs[0] * weights[0] + inputs[1] * weights[1] + inputs[2] * weights[2]
-output += bias
-print(output)
+weights1 = [0.2, 0.8, -0.5, 1.0]
+weights2 = [0.5, -0.91, 0.26, -0.5]
+weights3 = [-0.26, -0.27, 0.17, 0.87]
+bias1 = 2   #simulating 4 inputs on 3 neurons
+bias2 = 3   #each neuron has its own unique weight set and bias
+bias3 = 0.5
+
+
+#output for the 3 neurons listed in an array in order
+output = [inputs[0] * weights1[0] + inputs[1] * weights1[1] + inputs[2] * weights1[2] + inputs[3] * weights1[3] + bias1,
+          inputs[0] * weights2[0] + inputs[1] * weights2[1] + inputs[2] * weights2[2] + inputs[3] * weights2[3] + bias2,
+          inputs[0] * weights3[0] + inputs[1] * weights3[1] + inputs[2] * weights3[2] + inputs[3] * weights3[3] + bias3]
+
+print(output) 
+
