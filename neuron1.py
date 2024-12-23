76c2d253f2982d35dcf7703c975023004eefd5e2
bryson32 <154479610+bryson32@users.noreply.github.com>
2024-12-22 22:12:18 -0800

    Second Layer

--- neuron1.py
+++ neuron1.py
@@ -13,8 +13,16 @@ weights = [[0.2, 0.8, -0.5, 1.0],
 
 biases = [2, 3, 0.5]
 
-layer_outputs = np.dot(inputs, np.array(weights).T) + biases
-print(layer_outputs) 
+weights2 = [[0.1, -0.14, 0.5],
+           [-0.5, 0.12, -0.33],
+           [-0.44, -0.73, -0.13]]
+
+biases2 = [-1, 2, -0.5]
+
+layer1_outputs = np.dot(inputs, np.array(weights).T) + biases
+layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + biases2
+
+print(layer1_outputs) 
 
 # layer_outputs = []  # Output of current layer
 # for neuron_weights, neuron_bias in zip(weights, biases):
