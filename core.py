19c65e507073081c5a92f94d528ab4cbb494ecdb
bryson32 <154479610+bryson32@users.noreply.github.com>
2024-12-23 17:48:58 -0800

    add ReLU, import dataset

--- core.py
+++ core.py
@@ -1,12 +1,16 @@
 import sys
 import numpy as np
 import matplotlib
+import nnfs
+from nnfs.datasets import spiral_data
 
-np.random.seed(0)
+nnfs.init()
 
 X = [[1, 2, 3, 2.5],    #batch of 3, X is formal variable for inputs
      [2.0, 5.0, -1.0, 2.0],
      [-1.5, 2.7, 3.3, -0.8]]
+
+X, Y = spiral_data(100, 3) #100 points per class, 3 classes
 
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
@@ -15,10 +19,13 @@ class Layer_Dense:
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
     
-layer1 = Layer_Dense(4, 5)
-layer2 = Layer_Dense(5, 2)
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)    
+
+layer1 = Layer_Dense(2, 5)
+activation1 = Activation_ReLU()
 
 layer1.forward(X)
-print(layer1.output)
-layer2.forward(layer1.output)
-print(layer2.output)
+activation1.forward(layer1.output)
+print(activation1.output)
