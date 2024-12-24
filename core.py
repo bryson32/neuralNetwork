f025735b4d6fad661eef0a82dceaa02c260cbb99
bryson32 <154479610+bryson32@users.noreply.github.com>
2024-12-23 21:42:37 -0800

    softmax + predictions (broken)
    needs optimization and loss function

--- core.py
+++ core.py
@@ -6,11 +6,13 @@ from nnfs.datasets import spiral_data
 
 nnfs.init()
 
-X = [[1, 2, 3, 2.5],    #batch of 3, X is formal variable for inputs
-     [2.0, 5.0, -1.0, 2.0],
-     [-1.5, 2.7, 3.3, -0.8]]
+# X = [[1, 2, 3, 2.5],    #batch of 3, X is formal variable for inputs
+#      [2.0, 5.0, -1.0, 2.0],
+#      [-1.5, 2.7, 3.3, -0.8]]
 
-X, Y = spiral_data(100, 3) #100 points per class, 3 classes
+# X, Y = spiral_data(100, 3) #100 points per class, 3 classes
+
+
 
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
@@ -23,9 +25,24 @@ class Activation_ReLU:
     def forward(self, inputs):
         self.output = np.maximum(0, inputs)    
 
-layer1 = Layer_Dense(2, 5)
+class Activation_Softmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis = 1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis = 1, keepdims=True)
+        self.output = probabilities
+        
+X, Y = spiral_data(100, 3)
+
+dense1 = Layer_Dense(2,3)
 activation1 = Activation_ReLU()
 
-layer1.forward(X)
-activation1.forward(layer1.output)
-print(activation1.output)
+dense2 = Layer_Dense(3,3)
+activation2 = Activation_Softmax()
+
+dense1.forward(X)
+activation1.forward(dense1.output)
+
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+
+print(activation2.output)
